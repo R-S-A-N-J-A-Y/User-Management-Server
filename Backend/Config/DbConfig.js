3a92d6a5fccce90931b773ede2e57be3f014d02b
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-04-15 21:00:38 +0530

    Connected to the mongodb Atlas.

---
+++ Backend/Config/DbConfig.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const userName = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const URL = process.env.MONGO_CLUSTER_URL;
+
+const ConnectDB = () => {
+  mongoose
+    .connect(
+      `mongodb+srv://${userName}:${password}@${URL}.mongodb.net/User-Management`
+    )
+    .then(() => console.log("conneted to Db..."))
+    .catch((err) => console.log("Error Connecting to DB ", err));
+};
+
+module.exports = ConnectDB;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const ConnectDB = require("./Backend/Config/DbConfig");
+
+ConnectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World.");
