61faa1aafffbe7f4b065e7c3ac9ad5020ec7314f
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-04-15 23:09:28 +0530

    Autheticating endpoint for username and password is completed.

--- Backend/Routes/Login.js
+++ Backend/Routes/Login.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const Route = express.Router();
-
-// Get The User By UserName and Password
-
 const { getByUserName } = require("../Controllers/GetUser");
 
+// Validate user - Check Username is avilable in mongobd or not
+Route.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const result = await getByUserName(username, password);
+    if (!result) return res.send("No User Found");
+    return res.send("User Found");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Get The User By UserName and Password
 Route.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -14,4 +24,5 @@ Route.post("/login", async (req, res) => {
     res.status(500).send(err.message);
   }
 });
+
 module.exports = Route;
